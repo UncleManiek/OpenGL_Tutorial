20d1dd1b0074d824af7d71af8ac4b62df8e5c778
VirginDestroyer <mariusz.pielat@gmail.com>
2019-02-06 15:20:50 +0100

    Add files via upload

---
+++ OpenglTutorials/mainwindow.cpp
@@ -0,0 +1,60 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+{
+    setSurfaceType(QWindow::OpenGLSurface);
+
+    QSurfaceFormat format;
+    format.setProfile(QSurfaceFormat::CompatibilityProfile);
+    format.setVersion(2,1);
+    setFormat(format);
+
+    context = new QOpenGLContext;
+    context->setFormat(format);
+    context->create();
+    context->makeCurrent(this);
+
+    openGLFunctions = context->functions();
+}
+
+MainWindow::~MainWindow()
+{
+
+}
+
+void MainWindow::initializeGL()
+{
+
+}
+
+void MainWindow::resizeGL(int w, int h)
+{
+
+}
+
+void MainWindow::paintGL()
+{
+    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_QUADS);
+
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f(0.5f, -0.5f);
+    glVertex2f(0.5f, 0.5f);
+    glVertex2f(-0.5f, 0.5f);
+
+    glEnd();
+
+    glFlush();
+}
+
+void MainWindow::resizeEvent(QResizeEvent *event)
+{
+
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    paintGL();
+}
